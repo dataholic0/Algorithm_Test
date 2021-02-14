dbd18cdc467705159595287bb96ba4f8426fab6f
Pak Jeonghu <67231569+PakJeonghu@users.noreply.github.com>
2021-02-14 14:11:17 +0900

    Update insert_delete_getRandom.py
    # change 'set' data structure to 'dict' for reduce time complexity in getRandom fuction
    [result]
    # Runtime: 112 ms, faster than 43.24% of Python3 online submissions for Insert Delete GetRandom O(1).
    # Memory Usage: 18.5 MB, less than 28.27% of Python3 online submissions for Insert Delete GetRandom O(1).

--- LeetCode/insert_delete_getRandom.py
+++ LeetCode/insert_delete_getRandom.py
@@ -1,31 +1,40 @@
-import random
-
 class RandomizedSet:
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        
-        self.structure = set()
-        
+        self.data = {}
+        self.data_val = []
 
     def insert(self, val: int) -> bool:
         """
         Inserts a value to the set. Returns true if the set did not already contain the specified element.
         """
-        if val in self.structure:
+        if val in self.data:
             return False
         else:
-            self.structure.add(val)
+            self.data_val.append(val)
+            self.data[val] = len(self.data_val)
             return True
+        
 
     def remove(self, val: int) -> bool:
         """
         Removes a value from the set. Returns true if the set contained the specified element.
         """
-        if val in self.structure:
-            self.structure.remove(val)
+        if val in self.data:
+            last_element_val = self.data_val[-1] # 6
+            last_element_index = self.data[last_element_val]-1 # index 2
+            
+            change_index = self.data[val]-1 # index 0
+            
+            self.data[last_element_val] = change_index+1 # 3-> 1
+            self.data_val[last_element_index], self.data_val[change_index] = val, last_element_val
+            
+            self.data.pop(val)
+            self.data_val.pop()
+            
             return True
         else:
             return False
@@ -35,6 +44,4 @@ class RandomizedSet:
         """
         Get a random element from the set.
         """
-        if self.structure:
-            temp = random.sample(self.structure,k=1)
-            return temp[0]
+        return random.sample(self.data_val, k= 1)[0]
